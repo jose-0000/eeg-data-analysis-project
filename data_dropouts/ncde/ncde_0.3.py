d630ff8e1c6c69546b7aff3448f6a830ae01d35d
jose-0000 <146619360+jose-0000@users.noreply.github.com>
2025-07-19 00:14:02 +0530

    ncde with dropout 0.3

--- data_dropouts/ncde/ncde_0.3.py
+++ data_dropouts/ncde/ncde_0.3.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import torch
-import torch.nn as nn
+import torch.nn as nn 
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader, random_split
 from scipy.signal import spectrogram
